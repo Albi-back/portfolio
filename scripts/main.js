de34268544b0427e6a3eab435b42212858db324c
Albi-back <94167011+Albi-back@users.noreply.github.com>
2024-02-06 15:24:11 +0000

    inputs working

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,21 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
+const myImage = document.querySelector("img");
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+  }
+  if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Hello, ${myName}`;
+  }
+}
+myButton.onclick = () => {
+  setUserName();
+};
