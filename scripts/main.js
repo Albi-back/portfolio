cb7e5e45d245e573f5dc0aa2927fcbaaac210cdc
Albi-back <94167011+Albi-back@users.noreply.github.com>
2024-02-06 17:04:01 +0000

    animations fix

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,7 @@
 const myImage = document.querySelector("img");
 let myButton = document.querySelector("button");
 let myHeading = document.querySelector("h1");
+let myHeading2 = document.querySelector("h2");
 
-function setUserName() {
-  const myName = prompt("Please enter your name.");
-  if (!myName) {
-    setUserName();
-  } else {
-    localStorage.setItem("name", myName);
-  }
-  if (!localStorage.getItem("name")) {
-    setUserName();
-  } else {
-    const storedName = localStorage.getItem("name");
-    myHeading.textContent = `Hi, ${myName}, I'm Albi;`;
-  }
-}
-(function () {
-  setUserName();
-})();
+myHeading.textContent = `Hi, I'm Albi;`;
+myHeading2.textContent = `A programmer;`;
