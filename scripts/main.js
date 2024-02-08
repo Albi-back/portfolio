92ea97531b41c8eeaa81ff67a2771aa02349fc6b
Albi-back <94167011+Albi-back@users.noreply.github.com>
2024-02-08 23:38:45 +0000

    cpp page done

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,5 @@
 let myHeading = document.querySelector("h1.page1");
 let myHeading2 = document.querySelector("h2.page1");
-let myHeading3 = document.querySelector("h1.page2");
 
 myHeading.textContent = `Hi, I'm Albi;`;
-myHeading2.textContent = `A Developer;`;
-myHeading3.textContent = `My projects`;
+myHeading2.textContent = `A Game Developer;`;
