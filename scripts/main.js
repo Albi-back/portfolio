add879ed5e4c019a3e16dd791980891f7f83535d
Albi-back <94167011+Albi-back@users.noreply.github.com>
2024-02-08 17:02:51 +0000

    fixed background

--- scripts/main.js
+++ scripts/main.js
@@ -3,5 +3,5 @@ let myHeading2 = document.querySelector("h2.page1");
 let myHeading3 = document.querySelector("h1.page2");
 
 myHeading.textContent = `Hi, I'm Albi;`;
-myHeading2.textContent = `A programmer;`;
+myHeading2.textContent = `A Developer;`;
 myHeading3.textContent = `My projects`;
